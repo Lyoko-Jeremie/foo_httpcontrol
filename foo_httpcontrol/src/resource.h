413cc979ca816bd0bb7760de8a12ca8e926ecc15
oblikoamorale <devnull@localhost>
2015-03-13 13:08:53 +0000

    Imported from SVN by Bitbucket

---
+++ foo_httpcontrol/src/resource.h
@@ -0,0 +1,30 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_TAB1                                101
+#define IDD_TAB3                                104
+#define IDC_INTERFACE                           1001
+#define IDC_PORT                                1002
+#define IDC_CONTROL_IP                          1003
+#define IDC_CONTROL_CREDENTIALS                 1012
+#define IDC_CONTROL_USERNAME                    1013
+#define IDC_CONTROL_PASSWORD                    1014
+#define IDC_STATIC_USERNAME                     1015
+#define IDC_STATIC_PASSWORD                     1016
+#define IDC_CONTROL_PATH                        1018
+#define IDC_HIDE_NONPLAYABLES                   1019
+#define IDC_LOG_ACCESS                          1020
+#define IDC_SERVER_ROOT                         1021
+#define IDC_EXTRA_FORMATS                       1022
+#define IDC_AUTOPLAYLIST_SENDTODEDICATED        1033
+#define IDC_AUTOPLAYLIST_SORT_PATTERN           1034
+#define IDC_SERVERSTARTED                       1035
+#define IDC_AUTOPLAYLIST_QUERY_STEP1            1036
+#define IDC_AUTOPLAYLIST_QUERY_STEP2            1037
+#define IDC_AUTOPLAYLIST_QUERY_STEP3            1039
+#define IDC_STOP_AFTER_QUEUE_ENABLE             1040
+#define IDC_SERVER_ROOT_BTN                     1041
+#define IDC_GZIP_ENABLE                         1042
+#define IDC_LAUNCH_BROWSER                      1046
+#define IDC_IGNORED_FORMATS                     1048
